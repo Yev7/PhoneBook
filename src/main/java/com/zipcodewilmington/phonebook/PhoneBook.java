129fa7d911b656bd3f3066db45845b7d0b8cd374
Yevgeny Carranza <lijiu001@gmail.com>
2022-04-18 15:32:09 -0400

    First

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,64 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook= new HashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        phonebook.put(name, List.of(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        phonebook.put(name, List.of(phoneNumbers));
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return this.phonebook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return this.phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        for (Map.Entry<String, List<String>> entry: phonebook.entrySet()){
+            if (entry.getValue().contains(phoneNumber)){
+                return entry.getKey();
+            }
+        }
+        return "Error";
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> allNames = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry: phonebook.entrySet()){
+            allNames.add(entry.getKey());
+        }
+        return allNames;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.phonebook;
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Yev", "302-123-4567");
+        phoneBook.addAll("Jeff", "302-238-1832", "215-135-940");
+        phoneBook.add("Steve", "610-270-2840");
+        System.out.println(phoneBook.getAllContactNames());
+        phoneBook.remove("Steve");
+        System.out.println(phoneBook.getAllContactNames());
+        System.out.println(phoneBook.lookup("Yev"));
+        System.out.println(phoneBook.hasEntry("Jeff"));
+        System.out.println(phoneBook.reverseLookup("215-135-940"));
     }
 }
